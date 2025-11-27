4d2c9f3e9081f2394080e7db2e8dc97b18f2f328
Shariful Islam <sharifahmadwd@gmail.com>
2025-11-27 12:07:52 +0600

    vescel added

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://cheesy-bites-client.vercel.app"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ezpklr.mongodb.net/?appName=Cluster0`;
